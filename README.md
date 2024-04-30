b05601abf1ffde9db4bee9d97e3a21eadd495e10
joeypercia <jperc004@ucr.edu>
2024-04-30 08:13:05 -0700

    Prepare data

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Fake News Classifier
+
+This project uses machine learning to classify news articles into one of two categories: "reliable" or "fake".
+
+## Data Collection/Cleaning
+### Dataset: Fake News Corpus (https://github.com/several27/FakeNewsCorpus)
+
+* Drop all but 3 columns, “title”, “type” (fake or real), and “content”.
+* Remove all articles labeled as “political” or “proceed with caution”, their reliability is uncertain so they shouldn’t be used for training/testing.
+* Aggregate all article types labeled as satire, bias, rumor, conspiracy, junksci, hate, and clickbait into one type: “fake” (may lead to underfitting, but having to classify too many types could lead to overfitting. Will change as needed when testing the model, simpler is better for now)
+* Drop 50% of the fake articles, since there are twice as many in the dataset (3,835,102) vs. reliable articles (1,920,139). We don’t want our model to be biased towards predicting news as fake.
+* Clean the dataset: remove entries with any NULL fields, or have an “unknown” type.
+* Split the final dataframe into three different sets: 80% training, 10% validation, 10% testing, and output them to separate files in parquet format.
+
+### Building the AI Model:
+* Data preparation: transform text from datasets into features using a method such as TF-IDF: https://spark.apache.org/docs/1.6.0/ml-features.html#feature-extractors
+* Train the model with the training dataset using binomial logistic regression: https://spark.apache.org/docs/latest/ml-classification-regression.html#binomial-logistic-regression
+* After training the model, use k-fold cross validation to validate the model with the validation dataset after training: https://spark.apache.org/docs/latest/ml-tuning.html
+* After the model is trained and validated, test it on the testing dataset:
+https://stackoverflow.com/questions/45681387/predict-test-data-using-model-based-on-training-data-set
+
+
  
---
+++ data_collection.py
@@ -0,0 +1,115 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, when, rand
+from pyspark.sql.types import StructType, StructField, StringType
+import time
+
+
+def print_type_counts(df, name):
+    filtered_df = df.filter((col("type") == "fake") | (col("type") == "reliable"))
+    count_df = filtered_df.groupBy("type").count()
+    print(f"Counts of 'fake' and 'reliable' in {name}:")
+    count_df.show()
+
+
+start_time = time.time()
+
+spark = SparkSession.builder \
+    .appName("Data Preprocessing") \
+    .config("spark.executor.memory", "8g") \
+    .config("spark.driver.memory", "8g") \
+    .config("spark.executor.instances", "3") \
+    .getOrCreate()
+
+
+schema = StructType([
+    StructField("id", StringType(), True),
+    StructField("domain", StringType(), True),
+    StructField("type", StringType(), True),
+    StructField("url", StringType(), True),
+    StructField("content", StringType(), True),
+    StructField("scraped_at", StringType(), True),
+    StructField("inserted_at", StringType(), True),
+    StructField("updated_at", StringType(), True),
+    StructField("title", StringType(), True),
+    StructField("authors", StringType(), True),
+    StructField("keywords", StringType(), True),
+    StructField("meta_keywords", StringType(), True),
+    StructField("meta_description", StringType(), True),
+    StructField("tags", StringType(), True),
+    StructField("summary", StringType(), True),
+    StructField("source", StringType(), True)
+])
+
+df = spark.read.format("csv") \
+    .option("header", "true") \
+    .option("delimiter", ",") \
+    .option("multiLine", "true") \
+    .option("escape", "\"") \
+    .option("quote", "\"") \
+    .option("inferSchema", "true") \
+    .load("news_cleaned_2018_02_13.csv")
+
+print("df:")
+df.show(100)
+
+df_filtered = df.select("title", "type", "content")
+
+print("df_filtered:")
+df_filtered.show(100)
+
+df_filtered = df_filtered.filter(~col("type").isin(["political", "unreliable"]))
+
+fake_types = ["satire", "bias", "rumor", "conspiracy", "junksci", "hate", "clickbait", "fake"]
+df_filtered = df_filtered.withColumn("type", when(col("type").isin(fake_types), "fake").otherwise(col("type")))
+
+fake_df = df_filtered.filter(col("type") == "fake").sample(fraction=0.5, seed=42)
+real_df = df_filtered.filter(col("type") != "fake")
+
+final_df = fake_df.union(real_df)
+final_df = final_df.dropDuplicates(['title', 'content'])
+final_df = final_df.orderBy(rand())
+final_df = final_df.filter(
+    (col("title").isNotNull()) &
+    (col("type").isNotNull()) &
+    (col("type") != "unknown") &
+    (col("content").isNotNull())
+)
+
+train_df, val_df, test_df = final_df.randomSplit([0.8, 0.1, 0.1], seed=42)
+
+print("train_df:")
+train_df.show(100)
+print("val_df:")
+val_df.show(100)
+print("test_df:")
+test_df.show(100)
+
+train_df.write.parquet("train_data.parquet", mode='overwrite')
+val_df.write.parquet("val_data.parquet", mode='overwrite')
+test_df.write.parquet("test_data.parquet", mode='overwrite')
+
+print("val_data.parquet:")
+df = spark.read.format("parquet").load("val_data.parquet")
+df.show(100)
+
+print("train_data.parquet:")
+df = spark.read.format("parquet").load("train_data.parquet")
+df.show(100)
+
+print("test_data.parquet:")
+df = spark.read.format("parquet").load("test_data.parquet")
+df.show(100)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Data preparation execution time: {elapsed_time:.2f} seconds")
+
+print_type_counts(train_df, "Training Dataset")
+print_type_counts(val_df, "Validation Dataset")
+print_type_counts(test_df, "Test Dataset")
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Total execution time: {elapsed_time:.2f} seconds")
+
+spark.stop()
